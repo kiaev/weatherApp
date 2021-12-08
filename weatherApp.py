fcd6fd9ce96097a82459067055aca7e8d130d633
Kiaev Ivan <dapa_maru@mail.ru>
2021-12-08 06:47:03 +0600

    changed weatherApp

--- weatherApp.py
+++ weatherApp.py
@@ -1,10 +1,22 @@
-import requests;
-import datetime;
+import requests
+import datetime
+import json
 
 getCurrentYear = str(datetime.datetime.now().year)
 getCurrentMonth = str(datetime.datetime.now().month)
 getYesterday = str(datetime.datetime.now().day -1) 
 print(getCurrentYear,getCurrentMonth,getYesterday)
-getLocation = (requests.get("https://www.metaweather.com/api/location/2123260/"+getCurrentYear+"/"+getCurrentMonth+"/"+getYesterday+"/")).text
+
+getWeatherParse = requests.get("https://www.metaweather.com/api/location/2123260/"+getCurrentYear+"/"+getCurrentMonth+"/"+getYesterday+"/")
+weatherParametrList = (json.dumps(getWeatherParse.json(), sort_keys=True, indent=4))
+
+
+
+
+"""
+getLocation = ((requests.get("https://www.metaweather.com/api/location/2123260/"+getCurrentYear+"/"+getCurrentMonth+"/"+getYesterday+"/")).text).split(",")
 print (getLocation)
 
+for element in getLocation:
+    print(element)
+"""
