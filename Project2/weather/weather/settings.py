06998a39f41316ecd0bb2e0179acf3687c716f4f
Kiaev Ivan <dapa_maru@mail.ru>
2022-04-17 21:50:53 +0600

    add requirements.txt cryptography==3.4.7

--- Project2/weather/weather/settings.py
+++ Project2/weather/weather/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-g(%&%($s(!dbx&^mxp0x&gir&^=s2@&223wjj6pw+*!e-r-#m#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
